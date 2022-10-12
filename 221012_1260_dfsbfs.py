b6d58c8de572209e78c8509f4c4d1722f53ce2c6
wwan13 <wwan13@naver.com>
2022-10-12 15:02:40 +0900

    [feat]: solve 1260

---
+++ 221012_1260_dfsbfs.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    graph[v].sort()
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+def bfs(graph, v, visited):
+    queue = deque([v])
+    visited[v] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        
+        graph[v].sort()
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+n, m, v = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [False] * (n+1)
+dfs(graph, v, visited)
+print()
+visited = [False] * (n+1)
+bfs(graph, v, visited)
+
+
+

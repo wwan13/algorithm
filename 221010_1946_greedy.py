e163b03bdc10d64db47a5136c4e80b9ed4e7377f
wwan13 <wwan13@naver.com>
2022-10-12 13:50:06 +0900

    [fix] solve 1946

--- 221010_1946_greedy.py
+++ 221010_1946_greedy.py
@@ -5,56 +5,23 @@ QUE NAME: 신입 사원
 QUE TYPE: greedy
 https://www.acmicpc.net/problem/1946
 """
-
-from operator import index
+import sys
+input = sys.stdin.readline
 
 testCases = int(input())
 for _ in range(testCases):
     applicants = int(input())
-    entireScore = []
-    for _ in range(applicants):
-        entireScore.append(list(map(int, input().split())))
+    scores = [list(map(int, input().split())) for _ in range(applicants)]
+    scores_asc = sorted(scores)
 
-    # subTop = []
-    # for i in range(2):
-        # subTop.append(min(entireScore, key=lambda x:x[i]))
-    sub1Top = min(entireScore, key=lambda x:x[0])
-    sub2Top = min(entireScore, key=lambda x:x[1])
-
-    entireScore.sort(key=lambda x:x[0])
-    indexNum = entireScore.index(sub2Top)
-    result = 2
-    for i in range(1,indexNum):
-        if entireScore[i][1] < sub1Top[1]:
+    top = 0
+    result = 1
+    for i in range(1, applicants):
+        if scores_asc[i][1] < scores_asc[top][1]:
+            top = i
             result += 1
-    # entireScore = entireScore[0:indexNum]
-    # entireScore.sort(key=lambda x:x[1])
-    # indexNum = entireScore.index(subTop[0])
-    # entireScore = entireScore[0:indexNum]
 
     print(result)
+    
 
-    # tmpScore = entireScore[:]
-    # topApplicants = []
-    # for i in range(2):
-    #     subjectTop = min(entireScore, key=lambda x:x[i])
-    #     topApplicants.append(subjectTop)
-    #     tmpScore.remove(subjectTop)
-
-    # delList = []
-    # for i in range(2):
-    #     if i == 0:
-    #         subject = 1
-    #     else:
-    #         subject = 0
-
-    #     for j in range(len(tmpScore)):
-    #         if tmpScore[j][subject] > topApplicants[i][subject]:
-    #             delList.append(tmpScore[j])
-    #     for j in delList:
-    #         tmpScore.remove(j)
-
-    #     delList.clear()
-
-    # result = len(tmpScore) + 2
-    # print(result)
+    

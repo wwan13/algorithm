3e913d0bae314aa437ecbaaacdcd34fe699bb4cd
wwan13 <wwan13@naver.com>
2022-10-15 17:08:28 +0900

    [fix]: solve 1012

--- 221014_1012_dfs.py
+++ 221014_1012_dfs.py
@@ -7,22 +7,26 @@ https://www.acmicpc.net/problem/1012
 """
 
 import sys
-from unittest import result
+from collections import deque
+
 input = sys.stdin.readline
-
-def dfs(data, x, y, visited):
+ 
+def bfs(data, x, y, visited):
     visited[x][y] = True
-    print("a")
+    queue = deque([[x,y]])
 
-    addList = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-    for add in addList:
-        new_x = x + add[0]
-        new_y = y + add[1]
+    while queue:
+        x1, y1 = map(int, queue.popleft())
 
-        if new_x < 0 or new_y < 0 or new_x >= len(data) or new_y >= len(data[0]):
-            continue
-        if visited[new_x][new_y] == False and data[new_x][new_y] == 1:
-            dfs(data, new_x, new_y, visited)
+        addList = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        for add in addList:
+            new_x = x1 + add[0]
+            new_y = y1 + add[1]
+            if new_x < 0 or new_y < 0 or new_x >= len(data) or new_y >= len(data[0]):
+                continue
+            if visited[new_x][new_y] == False and data[new_x][new_y] == 1:
+                queue.append([new_x, new_y])
+                visited[new_x][new_y] = True
 
 t = int(input())
 for _ in range(t):
@@ -35,12 +39,10 @@ for _ in range(t):
         data[y][x] = 1
 
     result = 0
-    print()
-    for i in range(n):
-        for j in range(m):
-            print(i, j)
+    for i in range(len(data)):
+        for j in range(len(data[0])):
             if visited[i][j] == False and data[i][j] == 1:
                 result += 1
-                dfs(data, x, y, visited)
+                bfs(data, i, j, visited)
 
     print(result)

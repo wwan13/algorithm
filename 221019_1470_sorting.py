81744e9f550b2bee166cfadd067ec8998cdc3dbb
wwan13 <wwan13@naver.com>
2022-10-19 15:53:10 +0900

    [feat]: solve 2470

---
+++ 221019_1470_sorting.py
@@ -0,0 +1,42 @@
+"""
+DATE: 2022.10.19
+QUE NUM: 1470
+QUE NAME: 두 용액
+QUE TYPE: sorting
+https://www.acmicpc.net/problem/1470
+"""
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+data = list(map(int, input().split()))
+
+# n = 5
+# data = [-2, 4, -99, -1, 98]
+
+data.sort()
+
+# 최초 데이터 설졍
+left = 0
+right = n-1
+min = abs(data[0] + data[1])
+resultData = [0,0]
+
+# 모든 데이터에 대해
+while left < right:
+    d_left = data[left]
+    d_right = data[right]
+
+    sum = d_left + d_right
+
+    if abs(sum) <= min:
+        min = abs(sum)
+        resultData = [d_left, d_right]
+
+    if sum < 0:
+        left += 1
+    else:
+        right -= 1
+
+print(resultData[0], resultData[1])

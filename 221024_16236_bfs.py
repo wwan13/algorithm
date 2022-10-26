c2b5f3c136a386d8c048405b7df295a4e00c558e
wwan13 <wwan13@naver.com>
2022-10-26 16:51:02 +0900

    [feat]: solve 12623

--- 221024_16236_bfs.py
+++ 221024_16236_bfs.py
@@ -8,67 +8,91 @@ https://www.acmicpc.net/problem/16236
 
 import sys
 from collections import deque
+from copy import deepcopy
+
 input = sys.stdin.readline
+dxdy = [[0,1], [-1,0], [1,0], [0,-1]]
+
+class Shark:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.level = 2
+        self.experience = 0
+
+class Fish:
+    def __init__(self, x, y, distance):
+        self.x = x
+        self.y = y
+        self.distance = distance
+
 
 # 먹을 수 있는 물고기중 가장 가까운 물고기를 찾는 함수, 없으면 false를 반환
-def find_closest_fish(shark_x, shark_y, shark_level):
-    queue = deque([[shark_x, shark_y]])
+def find_closest_fish(shark):
+    queue = deque([[shark.x, shark.y]])
     visited = [[False] * n for _ in range(n)]
-    dxdy = [[0,1], [-1,0], [1,0], [0,-1]]
+    tmp_data = deepcopy(data)
+    eatable_fish_list = []
+
     while queue:
         x1, y1 = map(int, queue.popleft())
         visited[x1][y1] = True
+
         for x,y in dxdy:
             new_x = x1 + x
             new_y = y1 + y
 
+            # 위치 초과 처리
             if new_x < 0 or new_x >= n or new_y < 0 or new_y >= n:
                 continue
-            if data[new_x][new_y] <= shark_level and visited[new_x][new_y] == False:
+
+            if tmp_data[new_x][new_y] <= shark.level and visited[new_x][new_y] == False:
                 queue.append([new_x,new_y])
                 visited[new_x][new_y] = True
-                if data[new_x][new_y] < shark_level and data[new_x][new_y] != 0:
-                    return new_x, new_y
-    return False
+
+                if tmp_data[new_x][new_y] < shark.level and tmp_data[new_x][new_y] != 0:
+                    distance = abs(shark.x - new_x) + abs(shark.y - new_y)
+                    fish = Fish(new_x, new_y, distance)
+                    eatable_fish_list.append(fish)
+                    tmp_data[new_x][new_y] = 0
+
+    if len(eatable_fish_list) == 0:
+        return False
+    else:
+        eatable_fish_list.sort(key=lambda x: (x.distance, x.x, x.y))
+        return eatable_fish_list[0]
+
 
 # n = int(input())
 # data = [list(map(int, input().split())) for _ in range(n)]
-# n = 4
-# data = [[4,3,2,1], [0,0,0,0], [0,0,9,0], [1,2,3,4]]
+n = 4
+data = [[4,3,2,1], [0,0,0,0], [0,0,9,0], [1,2,3,4]]
 # data = [[0,0,0,0], [0,0,0,0], [0,0,9,0], [0,0,0,0]
-n = 6
-data = [[5,4,3,2,3,4], [4,3,2,3,4,5], [3,2,9,5,6,6], [2,1,2,3,4,5], [3,2,1,6,5,4], [6,6,6,6,6,6]]
+# n = 6
+# data = [[5,4,3,2,3,4], [4,3,2,3,4,5], [3,2,9,5,6,6], [2,1,2,3,4,5], [3,2,1,6,5,4], [6,6,6,6,6,6]]
 
-print(*data, sep="\n")
+# print(*data, sep="\n")
 
-shark_level = 2
-shark_y, shark_x = map(int,[(i,j) for i in range(n) for j in range(n) if data[i][j]==9][0])
+shark_x, shark_y = map(int,[(i,j) for i in range(n) for j in range(n) if data[i][j]==9][0])
+shark = Shark(shark_x, shark_y)
 time = 0 # 소요 시간
-experience = 0 # 경험치
-
-print(shark_x,shark_y)
-x, y = find_closest_fish(shark_x, shark_y, shark_level)
-# print(x,y)
 
 while(True):
-    if find_closest_fish(shark_x, shark_x, shark_level) == False:
+    fish = find_closest_fish(shark)
+    if fish == False:
         break
+    
+    time += fish.distance
 
-    # 먹을 수 있는 가장 가까운 물고기를 찾음
-    fish_x, fish_y = find_closest_fish(shark_x, shark_y, shark_level)
-    print(fish_x,fish_y)
-    # 거리 계산 후 시간에 더함
-    distance = abs(shark_x - fish_x) + abs(shark_y - fish_y)
-    time += distance
-    experience += 1
-    # 먹은 물고기는 없애버림
-    data[fish_x][fish_y] = 0
-    shark_x = fish_x
-    shark_y = fish_y
+    shark.experience += 1
 
-    # 레벨업
-    if experience >= shark_level:
-        shark_level += 1
-        experience = 0
+    if shark.experience >= shark.level:
+        shark.level += 1
+        shark.experience = 0
+    
+    shark.x = fish.x
+    shark.y = fish.y
+
+    data[fish.x][fish.y] = 0
 
 print(time)

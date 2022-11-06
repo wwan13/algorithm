11713905db91a847f0e197be78e014083db33f23
wwan13 <wwan13@naver.com>
2022-11-06 22:01:24 +0900

    [feat] solve 12866

--- 221106_12865_dp.py
+++ 221106_12865_dp.py
@@ -9,18 +9,18 @@ https://www.acmicpc.net/problem/12865
 import sys
 input = sys.stdin.readline
 
-# n, k = map(int,input().split())
-# data = [list(map(int, input().split())) for _ in range(n)]
-
-n = 4
-k = 7
-data = [[6, 13], [4,8], [3,6], [5, 12]]
-
+n, k = map(int,input().split())
+data = [list(map(int, input().split())) for _ in range(n)]
+data.insert(0,[0,0])
 dp = [[0 for _ in range(k + 1)] for _ in range(n+1)]
-
 
 for i in range(1, n+1):
     weight = data[i][0]
     value = data[i][1]
     for j in range(1, k+1):
-        pass
+        if j < weight:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(value + dp[i-1][j - weight], dp[i-1][j])
+
+print(dp[n][k])

011be991522c9d343b455d57685c7cc6b791f581
wwan13 <wwan13@naver.com>
2022-11-07 21:34:14 +0900

    [feat]: solve 2156

---
+++ 221107_2156_dp.py
@@ -0,0 +1,23 @@
+"""
+DATE: 2022.11.07
+QUE NUM: 2146
+QUE NAME: 포도주 시식
+QUE TYPE: dynamic programming
+https://www.acmicpc.net/problem/2156
+"""
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+data = [int(input()) for _ in range(n)]
+
+dp = [0] * n
+
+dp[0] = data[0]
+dp[1] = data[0] + data[1]
+dp[2] = max(data[0] + data[2], data[1] + data[2], dp[1])
+for i in range(3, n):
+    dp[i] = max(dp[i-2] + data[i], dp[i-1], data[i] + data[i-1] + dp[i-3])
+
+print(max(dp))

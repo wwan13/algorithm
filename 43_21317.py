d03fbd7903415bd7a0a3ee658350c1b4db370722
wwan13 <wwan13@naver.com>
2023-04-13 00:39:26 +0900

    [feat]: solve 21417

--- 43_21317.py
+++ 43_21317.py
@@ -15,30 +15,29 @@ k = int(input())
 
 dp = [0] * (n+1)
 
-if n == 1:
+if n==1:
     print(0)
-    exit()
 elif n==2:
     print(energies[1][0])
-    exit()
+else:
+    dp[2] = energies[1][0]
+    dp[3] = min(dp[2] + energies[2][0], energies[1][1])
 
-dp[2] = energies[1][0]
-dp[3] = min(dp[2] + energies[2][0], energies[1][1])
+    for i in range(4, n+1):
+        dp[i] = min(dp[i-1] + energies[i-1][0], dp[i-2] + energies[i-2][1])
 
-for i in range(4, n+1):
-    dp[i] = min(dp[i-1] + energies[i-1][0], dp[i-2] + energies[i-2][1])
+    _min = dp[-1]
+    for i in range(1, n-2):
+        dp_copy = dp.copy()
+        print(dp_copy)
 
-_min = dp[-1]
-for i in range(1, n-2):
-    dp_copy = dp.copy()
+        if dp_copy[i] + k < dp_copy[i+3]:
+            dp_copy[i+3] = dp_copy[i] + k
 
-    if dp_copy[i] + k < dp_copy[i+3]:
-        dp_copy[i+3] = dp_copy[i] + k
+            for j in range(i+4, n+1):
+                dp_copy[j] = min(dp_copy[j], dp_copy[j-1] + dp[j-1][0], dp_copy[j-2] + dp[j-2][1])
 
-        for j in range(i+4, n+1):
-            dp_copy[j] = min(dp_copy[j], dp_copy[j-1] + dp[j-1][0], dp_copy[j-2] + dp[j-2][1])
+            if _min > dp_copy[-1]:
+                _min = dp_copy[-1]
 
-        if _min > dp_copy[-1]:
-            _min = dp_copy[-1]
-
-print(_min)
+    print(_min)

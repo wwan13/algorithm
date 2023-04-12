8947ece94dcae1c0c26e5fb1fb14ae5bb41602b1
wwan13 <wwan13@naver.com>
2023-04-12 23:58:43 +0900

    [feat]: 21317 type error

---
+++ 43_21317.py
@@ -0,0 +1,44 @@
+"""
+DATE: 2023.04.12
+QUE NUM: 21317
+QUE NAME: 징검다리 건너기
+QUE TYPE: dynamic programming
+https://www.acmicpc.net/problem/21317
+"""
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+energies = [[0,0]] + [list(map(int, input().split())) for _ in range(n-1)]
+k = int(input())
+
+dp = [0] * (n+1)
+
+if n == 1:
+    print(0)
+    exit()
+elif n==2:
+    print(energies[1][0])
+    exit()
+
+dp[2] = energies[1][0]
+dp[3] = min(dp[2] + energies[2][0], energies[1][1])
+
+for i in range(4, n+1):
+    dp[i] = min(dp[i-1] + energies[i-1][0], dp[i-2] + energies[i-2][1])
+
+_min = dp[-1]
+for i in range(1, n-2):
+    dp_copy = dp.copy()
+
+    if dp_copy[i] + k < dp_copy[i+3]:
+        dp_copy[i+3] = dp_copy[i] + k
+
+        for j in range(i+4, n+1):
+            dp_copy[j] = min(dp_copy[j], dp_copy[j-1] + dp[j-1][0], dp_copy[j-2] + dp[j-2][1])
+
+        if _min > dp_copy[-1]:
+            _min = dp_copy[-1]
+
+print(_min)

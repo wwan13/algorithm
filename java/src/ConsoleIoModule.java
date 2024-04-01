f67902c73f5397f994ec8c772d43aea9d55ac60f
wwan13 <wwan13@naver.com>
2024-04-01 09:55:54 +0900

    feat : console 입출력 모듈

---
+++ java/src/ConsoleIoModule.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleIoModule {
+
+    public static class Console {
+
+        private final static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        public static int nextInt() {
+            try {
+                return Integer.parseInt(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public static String readLine() {
+            try {
+                return reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public static char[] readArray(int size) {
+            char[] data = new char[size];
+            String line = Boj4396.Console.readLine();
+            for (int i = 0; i < size; i++) {
+                data[i] = line.charAt(i);
+            }
+            return data;
+        }
+
+        public static char[][] readDoubleArray(int rowSize, int columnSize) {
+            char[][] data = new char[rowSize][columnSize];
+            for (int i = 0; i < rowSize; i++) {
+                String line = Boj4396.Console.readLine();
+                for (int j = 0; j < columnSize; j++) {
+                    data[i][j] = line.charAt(j);
+                }
+            }
+            return data;
+        }
+
+        public static void print(String data) {
+            System.out.println(data);
+        }
+    }
+}

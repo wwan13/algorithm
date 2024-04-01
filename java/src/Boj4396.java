37b57ea4b33fdd2bc326f6b9d33ea16ec49cea8b
wwan13 <wwan13@naver.com>
2024-04-01 10:59:08 +0900

    solve : 4396 지뢰찾기

--- java/src/Boj4396.java
+++ java/src/Boj4396.java
@@ -16,14 +16,66 @@ public class Boj4396 {
 
     private static char[][] solution(int n, char[][] originalMap, char[][] openedMap) {
 
-        Console.print(String.valueOf(originalMap[0][4]));
+        final char MINE = '*';
+        final char NOT_MINE = '.';
+
+        final char OPENED = 'x';
+        final char NOT_OPENED = '.';
 
         char[][] answer = new char[n][n];
+
+        boolean isCleared = false;
+
+        int[] dy = {-1, -1, 0, 1, 1, 1, 0, -1};
+        int[] dx = {0, 1, 1, 1, 0, -1, -1, -1};
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (openedMap[i][j] == OPENED) {
+                    char target = originalMap[i][j];
+                    if (target == MINE) {
+                        isCleared = true;
+                        continue;
+                    }
+
+                    int count = 0;
+                    for (int k = 0; k < 8; k++) {
+                        int ny = i + dy[k];
+                        int nx = j + dx[k];
+
+                        if ((nx >= 0 && nx < n) && (ny >= 0 && ny < n)) {
+                            if (originalMap[ny][nx] == MINE) {
+                                count += 1;
+                            }
+                        }
+                    }
+                    answer[i][j] = Character.forDigit(count, 10);
+                } else if (openedMap[i][j] == NOT_OPENED) {
+                    answer[i][j] = NOT_OPENED;
+                }
+            }
+        }
+
+        if (isCleared) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (originalMap[i][j] == MINE) {
+                        answer[i][j] = MINE;
+                    }
+                }
+            }
+        }
+
         return answer;
     }
 
     private static void displayResult(char[][] answer) {
-
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer[0].length; j++) {
+                Console.print(String.valueOf(answer[i][j]));
+            }
+            Console.lineBreak();
+        }
     }
 
     public static class Console {
@@ -67,7 +119,11 @@ public class Boj4396 {
         }
 
         public static void print(String data) {
-            System.out.println(data);
+            System.out.print(data);
+        }
+
+        public static void lineBreak() {
+            System.out.print("\n");
         }
     }
 }

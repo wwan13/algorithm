d2c1ca0c783714143e67511bdae6bc31ace24ea0
wwan13 <wwan13@naver.com>
2024-04-26 11:33:01 +0900

    feat : update readme style

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -14,11 +14,11 @@ def get_files_by_language(language):
 
 def get_problem_info(problem_id):
     levels = {
-        0: "`U`",
-        1: "`B5`", 2: "`B4`", 3: "`B3`", 4: "`B2`", 5: "`B1`",
-        6: "`S5`", 7: "`S4`", 8: "`S3`", 9: "`S2`", 10: "`S1`",
-        11: "`G5`", 12: "`G4`", 13: "`G3`", 14: "`G2`", 15: "`G1`",
-        16: "`P1`", 17: "`P2`", 18: "`P3`", 19: "`P2`", 20: "`P1`",
+        0: "U",
+        1: "B5", 2: "B4", 3: "B3", 4: "B2", 5: "B1",
+        6: "S5", 7: "S4", 8: "S3", 9: "S2", 10: "S1",
+        11: "G5", 12: "G4", 13: "G3", 14: "G2", 15: "G1",
+        16: "P1", 17: "P2", 18: "P3", 19: "P2", 20: "P1",
     }
 
     response = requests.get("https://solved.ac/api/v3/problem/show?problemId={}".format(problem_id)).json()
@@ -35,18 +35,18 @@ def get_solved_info(fileNmae):
     if fileNmae.startswith("boj_"):
         return {
             "problem_number": fileNmae.split("_")[1].split(".")[0],
-            "language": "`python`"
+            "language": "python"
         }
 
     if fileNmae.startswith("Boj"):
         return {
             "problem_number": fileNmae.split("Boj")[1].split(".")[0],
-            "language": "`java`"
+            "language": "java"
         }
 
     return {
             "problem_number": fileNmae,
-            "language": "`unknown`"
+            "language": "unknown"
         }
 
 
@@ -61,7 +61,7 @@ def create_markdown_file():
     readme.write("![Python](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white)  \n")
     readme.write("[![Solved.ac Profile](http://mazassumnida.wtf/api/v2/generate_badge?boj=wwan13)](https://solved.ac//)<br>\n\n")
     readme.write("</div>\n")
-    readme.write("<br/>\n")
+    readme.write("<br/>\n\n")
 
     readme.write("## 🌱 SOLVED PROBLEMS\n\n")
     readme.write("| NUM |  ID  | NAME | LEVEL | TAG | LANGUAGE |\n")
@@ -81,9 +81,9 @@ def create_markdown_file():
                 index,
                 problem_number,
                 problem_info["problem_name"],
-                problem_info["problem_level"],
+                "`{}`".format(problem_info["problem_level"]),
                 "`{}`".format(problem_info["problem_tag"]),
-                get_solved_info(file)["language"],
+                "`{}`".format(get_solved_info(file)["language"]),
             ))
 
     readme.write("\n<br/>\n")

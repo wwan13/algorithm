3aa12890c79ade4efe81c29258ea9dde056a3161
wwan13 <wwan13@naver.com>
2024-10-15 11:29:57 +0900

    solve : 18258 큐 2

---
+++ java/src/boj18258/Main.java
@@ -0,0 +1,60 @@
+package boj18258;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+
+        int last = 0;
+
+        for (int i = 0; i < n; i++) {
+            String[] command = readLine().split(" ");
+
+            switch (command[0]) {
+                case "push":
+                    last = Integer.parseInt(command[1]);
+                    queue.offer(last);
+                    break;
+                case "pop":
+                    if (queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.poll()).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(queue.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(queue.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "front":
+                    sb.append(queue.isEmpty() ? -1 : queue.peek()).append("\n");
+                    break;
+                case "back":
+                    sb.append(queue.isEmpty() ? -1 : last).append("\n");
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    private static String readLine() {
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

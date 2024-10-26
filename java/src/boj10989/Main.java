d5eee0ee74257b341b22fcf9402a9b05c4df45a3
wwan13 <wwan13@naver.com>
2024-10-27 02:01:07 +0900

    solve : 100989 수 정렬하기 3

---
+++ java/src/boj10989/Main.java
@@ -0,0 +1,31 @@
+package boj10989;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(readLine());
+        StringBuilder sb = new StringBuilder();
+
+        IntStream.range(0, n)
+                .map(it -> Integer.parseInt(readLine()))
+                .sorted()
+                .forEach(it -> sb.append(it).append("\n"));
+
+        System.out.println(sb);
+    }
+
+    private static String readLine() {
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
